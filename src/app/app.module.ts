7d26c508443d0cac8dce49a9def3906c5cecee53
Joseph Sungpil Choi <chousemath@gmail.com>
2019-09-12 12:52:55 +0900

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -12,7 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [NgbModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, NgbModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonSlides, {static: false}) slides: IonSlides;
+  public imgLeft = 'assets/images/navigate-left.png';
+  public imgRight = 'assets/images/navigate-right.png';
   public slideOpts = {
     initialSlide: 2,
     speed: 400
   };
   constructor() {}
-
+  public slidePrev() {
+    this.slides.slidePrev();
+  }
+  public slideNext() {
+    this.slides.slideNext();
+  }
 }
